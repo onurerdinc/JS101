259aa6fd3be0235b29ef85b2fdb6c5ea13d1c9d6
Onur Erdinc <onurerdnc@gmail.com>
2023-11-21 17:28:31 +0300

    second commit

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -6,28 +6,54 @@
 
 const readLine = require("readline-sync");
 
-console.log("Welcome to the calculator!");
+function invalidNumber(num){
+  return num.trimStart() === "" || Number.isNaN(Number(num));
+}
 
-console.log("What's the first number?");
+function prompt(msg){
+  console.log(`=> ${msg}`)
+};
+
+prompt("Welcome to the calculator!");
+
+prompt("What's the first number?");
 let number1 = readLine.question();
 
-console.log("What's the second number?");
+while (invalidNumber(number1)) {
+prompt("Hmmm.. that doesn't look like a valid number.")
+number1 = readLine.question();
+}
+
+prompt("What's the second number?");
 let number2 = readLine.question();
 
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+while (invalidNumber(number2)) {
+  prompt("Hmmm.. that doesn't look like a valid number.")
+  number2 = readLine.question();
+  }
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
 let operation = readLine.question();
+while (!operation.includes([1,2,3,4])) {
+  prompt("Must choose 1,2,3 or 4.")
+  operation = readLine.question();
+  }
+
 let output;
-if (operation === "1"){
-  output = Number(number1) + Number(number2);
-}
-else if (operation === "2"){
-  output = Number(number1) - Number(number2);
-}
-else if (operation === "3"){
-  output = Number(number1) * Number(number2);
-}
-else if (operation === "4"){
-  output = Number(number1) / Number(number2);
+switch (operation){
+  case '1': 
+   output = Number(number1) + Number(number2);
+   break;
+   case '2':
+    output = Number(number1) - Number(number2);
+    break;
+     case '3':
+      output = Number(number1) * Number(number2);
+    break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
 }
 
-console.log(`The result is ${output}`);
+
+prompt(`The result is ${output}.`);
